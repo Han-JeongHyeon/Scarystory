da787aec3b6aed762d5edd895f203be49d486c09
Han JeongHyeon <fldkrj131@gmail.com>
2024-07-03 17:33:01 +0900

    [feat] util imageDefault size 정의

--- app/src/main/java/com/horror/scarystory/Util/util.kt
+++ app/src/main/java/com/horror/scarystory/Util/util.kt
@@ -1,8 +1,6 @@
 package com.horror.scarystory.Util
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 
 val defaultColor = Color(0xFF252525)
@@ -10,3 +8,6 @@ val colorWhite = Color.White
 val settingColor = Color(0xFFACACAC)
 
 val topBarHeight = 50.dp
+
+val imageDefaultWidth = 250.dp
+val imageDefaultHeight = 220.dp
