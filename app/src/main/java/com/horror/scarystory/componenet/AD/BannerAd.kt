784cba8aac31f9bec2b2bd2df1fbbcc33ad31141
Han JeongHyeon <fldkrj131@gmail.com>
2024-07-03 17:35:23 +0900

    [feat] AD_ID properties 작업 진행

--- app/src/main/java/com/horror/scarystory/componenet/AD/BannerAd.kt
+++ app/src/main/java/com/horror/scarystory/componenet/AD/BannerAd.kt
@@ -1,20 +1,18 @@
-package com.horror.scarystory.componenet
+package com.horror.scarystory.componenet.AD
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.horror.scarystory.BuildConfig
 
 @Composable
 fun BannerAd() {
@@ -28,7 +26,7 @@ fun BannerAd() {
             factory = { context ->
                 AdView(context).apply {
                     setAdSize(AdSize.FULL_BANNER)
-                    adUnitId = "ca-app-pub-3940256099942544/6300978111"
+                    adUnitId = BuildConfig.AD_BANNER_ID
                     loadAd(AdRequest.Builder().build())
                 }
             },
